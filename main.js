ec72b1018682ee01ca0785df1f02f8ee58c32167
Maciej Treder <treder.mc@gmail.com>
2022-04-06 19:12:19 +0200

    end project

--- main.js
+++ main.js
@@ -1,52 +1,59 @@
 let btn = document.querySelector(".draw");
 let reset = document.querySelector(".reset");
-let sq1 = document.querySelector(".sq1");
-let sq2 = document.querySelector(".sq2");
-let sq3 = document.querySelector(".sq3");
-let sq4 = document.querySelector(".sq4");
-let sq5 = document.querySelector(".sq5");
-let sq6 = document.querySelector(".sq6");
-let sq7 = document.querySelector(".sq7");
-let sq8 = document.querySelector(".sq8");
-let sq9 = document.querySelector(".sq9");
+
+const segments = [
+    document.querySelectorAll(`div[data-segment="1"]`),
+    document.querySelectorAll(`div[data-segment="2"]`),
+    document.querySelectorAll(`div[data-segment="3"]`),
+];
+let square = document.querySelectorAll(".square");
+
+function reload() {
+    segments.forEach(segment => {
+        segment.forEach(square => {
+            square.classList.remove("green");
+        });
+    });
+}
 
 btn.addEventListener("click", () => {
+    reload();
     const number = Math.floor(Math.random() * 6 + 1);
 
-    if (number === 1) {
-        sq5.classList.add("green");
-    } else if (number === 2) {
-        sq7.classList.add("green");
-        sq3.classList.add("green");
-    } else if (number === 3) {
-        sq5.classList.add("green");
-        sq7.classList.add("green");
-        sq3.classList.add("green");
-    } else if (number === 4) {
-        sq1.classList.add("green");
-        sq3.classList.add("green");
-        sq7.classList.add("green");
-        sq9.classList.add("green");
-    } else if (number === 5) {
-        sq1.classList.add("green");
-        sq3.classList.add("green");
-        sq5.classList.add("green");
-        sq7.classList.add("green");
-        sq9.classList.add("green");
-    } else {
-        sq1.classList.add("green");
-        sq3.classList.add("green");
-        sq4.classList.add("green");
-        sq6.classList.add("green");
-        sq7.classList.add("green");
-        sq9.classList.add("green");
+    switch (number) {
+        case 1:
+            segments[1][1].classList.add("green");
+            break;
+        case 2:
+            segments[0][2].classList.add("green");
+            segments[2][0].classList.add("green");
+            break;
+        case 3:
+            segments[1][1].classList.add("green");
+            segments[2][0].classList.add("green");
+            segments[0][2].classList.add("green");
+            break;
+        case 4:
+            segments[0][0].classList.add("green");
+            segments[2][0].classList.add("green");
+            segments[0][2].classList.add("green");
+            segments[2][2].classList.add("green");
+            break;
+        case 5:
+            segments[0][0].classList.add("green");
+            segments[2][0].classList.add("green");
+            segments[0][2].classList.add("green");
+            segments[2][2].classList.add("green");
+            break;
+        case 6:
+            segments[0][0].classList.add("green");
+            segments[1][0].classList.add("green");
+            segments[2][0].classList.add("green");
+            segments[0][2].classList.add("green");
+            segments[1][2].classList.add("green");
+            segments[2][2].classList.add("green");
+            break;
     }
 
 })
 
-reset.addEventListener("click", () => {
-    function reload() {
-        location.reload();
-    }
-    reload();
-})
